5f604a7a92997ff6932369ed3f46ecf72c8e1c95
Omer Subasi <19795651+osubasi@users.noreply.github.com>
2025-09-04 03:14:26 -0400

    second

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -1,3 +1,16 @@
-def emotion_detector():
-    return
+import requests
+import json
 
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myinput = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url, json=myinput, headers=headers)
+    formatted_response = json.loads(response.text)
+    
+    if response.status_code == 200:
+        return formatted_response
+
+    elif response.status_code == 500:
+        return -1
